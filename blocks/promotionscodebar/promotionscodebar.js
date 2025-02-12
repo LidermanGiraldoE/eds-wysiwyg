b0bdeb2504a12fb88d448d0219161ee6ced04efb
LidermanGiraldoE <liderman.giraldo@omni.pro>
2025-02-12 09:52:53 -0500

    Fix

--- blocks/promotionscodebar/promotionscodebar.js
+++ blocks/promotionscodebar/promotionscodebar.js
@@ -1,19 +1,16 @@
 export default function decorate(block) {
-    console.log('decorate promotionscodebar', block);
-  
-    const container = document.createElement('div');
-    container.classList.add('promotionscodebar-list');
-  
-    [...block.children].forEach((row) => {
-      const item = document.createElement('a');
-      item.classList.add('promotionscodebar-item');
-      while (row.firstElementChild) {
-        item.append(row.firstElementChild);
-      }
-      container.append(item);
-    });
-  
-    block.textContent = '';
-    block.append(container);
-  }
-  
+  const container = document.createElement('div');
+  container.classList.add('promotionscodebar-list');
+
+  [...block.children].forEach((row) => {
+    const item = document.createElement('a');
+    item.classList.add('promotionscodebar-item');
+    while (row.firstElementChild) {
+      item.append(row.firstElementChild);
+    }
+    container.append(item);
+  });
+
+  block.textContent = '';
+  block.append(container);
+}
