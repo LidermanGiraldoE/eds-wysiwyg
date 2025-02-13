edfbe6b1b4bdaf386b7eace809dbdf53a22f2d44
LidermanGiraldoE <liderman.giraldo@omni.pro>
2025-02-13 15:37:31 -0500

    Primera version banner principal

---
+++ blocks/ulta-banner-principal/ulta-banner-principal.js
@@ -0,0 +1,34 @@
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  block.classList.add('ulta-banner');
+
+  const [imageContainer, taglineContainer, titleContainer, descriptionContainer] = block.children;
+
+  if (imageContainer) {
+    imageContainer.classList.add('ulta-banner-wrapper');
+    const img = imageContainer.querySelector('img');
+    moveInstrumentation(imageContainer, img);
+    if (img) img.classList.add('ulta-banner-img');
+  }
+
+  const textsContainer = document.createElement('div');
+  textsContainer.classList.add('ulta-banner-texts');
+
+  if (taglineContainer) {
+    taglineContainer.classList.add('ulta-banner-tagline');
+    textsContainer.appendChild(taglineContainer);
+  }
+
+  if (titleContainer) {
+    titleContainer.classList.add('ulta-banner-title');
+    textsContainer.appendChild(titleContainer);
+  }
+
+  if (descriptionContainer) {
+    descriptionContainer.classList.add('ulta-banner-description');
+    textsContainer.appendChild(descriptionContainer);
+  }
+
+  block.appendChild(textsContainer);
+}
