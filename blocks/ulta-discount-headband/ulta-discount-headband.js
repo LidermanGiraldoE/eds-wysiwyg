b6adb18c6745f034852a1547a19f7fb0da77f76c
JTorresV17 <juan.torres@omni.pro>
2025-03-07 12:22:07 -0500

    bloque ulta-discount-headband

--- blocks/ulta-discount-headband/ulta-discount-headband.js
+++ blocks/ulta-discount-headband/ulta-discount-headband.js
@@ -13,37 +13,40 @@ export default function decorate(block) {
   const linkUrlElement = items.shift(); // URL del link
   const backgroundColorElement = items.shift(); // Color de fondo
 
-  // Obtener valores de los elementos
-  const discountText = discountTextElement?.textContent.trim() || '';
-  const textColor = textColorElement?.textContent.trim() || '#CC0058';
-  const discountCode = discountCodeElement?.textContent.trim() || '';
-  const linkText = linkTextElement?.textContent.trim() || '';
-  const linkUrl = linkUrlElement?.querySelector('a')?.href || '#';
-  const backgroundColor = backgroundColorElement?.textContent.trim() || '#FBF1F3';
+  // Obtener valores directamente desde el contenido del AEM
+  const discountText = discountTextElement?.innerHTML || '';
+  const textColor = textColorElement?.textContent.trim() || '';
+  const discountCode = discountCodeElement?.innerHTML || '';
+  const linkText = linkTextElement?.innerHTML || '';
+  const linkUrl = linkUrlElement?.querySelector('a')?.href || '';
+  const backgroundColor = backgroundColorElement?.textContent.trim() || '';
 
   // Crear el contenedor principal del cintillo de descuento
   const discountContent = document.createElement('div');
   discountContent.classList.add('ulta-discount-headband');
   discountContent.style.backgroundColor = backgroundColor;
+  discountContent.style.color = textColor;
 
   // Manejo del texto del descuento
   const discountTextDiv = document.createElement('div');
   discountTextDiv.classList.add('ulta-discount-headband-text');
   discountTextDiv.style.color = textColor;
-  discountTextDiv.textContent = discountText;
+  discountTextDiv.innerHTML = discountText;
   moveInstrumentation(discountTextElement, discountTextDiv);
 
   // Manejo del código de descuento
   const discountCodeDiv = document.createElement('div');
   discountCodeDiv.classList.add('ulta-discount-headband-code');
-  discountCodeDiv.textContent = discountCode;
+  discountCodeDiv.style.color = textColor;
+  discountCodeDiv.innerHTML = discountCode;
   moveInstrumentation(discountCodeElement, discountCodeDiv);
 
   // Manejo del enlace
   const discountLink = document.createElement('a');
   discountLink.classList.add('ulta-discount-headband-link');
   discountLink.href = linkUrl;
-  discountLink.textContent = linkText;
+  discountLink.style.color = textColor;
+  discountLink.innerHTML = linkText;
   moveInstrumentation(linkTextElement, discountLink);
 
   // Estructura final del bloque
