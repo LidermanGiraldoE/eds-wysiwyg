05d73982b4c803d7a15da018227101b36e2536b7
LidermanGiraldoE <liderman.giraldo@omni.pro>
2025-04-16 11:48:53 -0500

    feat: Update discount headband block styling and configuration

--- blocks/discount-headband/discount-headband.js
+++ blocks/discount-headband/discount-headband.js
@@ -20,37 +20,43 @@ export default function decorate(block) {
   const backgroundColor = backgroundColorElement?.textContent.trim() || '';
 
   // Asignar el background color a la clase existente
-  block.closest('.ulta-discount-headband-wrapper')?.style.setProperty('background-color', backgroundColor);
+  block.closest('.discount-headband')?.style.setProperty('background-color', backgroundColor);
 
   // Crear el contenedor principal del cintillo de descuento
   const discountContent = document.createElement('div');
-  discountContent.classList.add('ulta-discount-headband');
-  discountContent.style.backgroundColor = backgroundColor;
+  discountContent.classList.add('discount-headband-content');
   discountContent.style.color = textColor;
+
+  // Crear un contenedor que agrupe el texto del descuento y el código
+  const discountInfoContainer = document.createElement('div');
+  discountInfoContainer.classList.add('discount-headband-info');
 
   // Manejo del texto del descuento
   const discountTextDiv = document.createElement('div');
-  discountTextDiv.classList.add('ulta-discount-headband-text');
+  discountTextDiv.classList.add('discount-headband-text');
   discountTextDiv.style.color = textColor;
   discountTextDiv.innerHTML = discountText;
   moveInstrumentation(discountTextElement, discountTextDiv);
 
   // Manejo del código de descuento
   const discountCodeDiv = document.createElement('div');
-  discountCodeDiv.classList.add('ulta-discount-headband-code');
+  discountCodeDiv.classList.add('discount-headband-code');
   discountCodeDiv.innerHTML = discountCode;
   moveInstrumentation(discountCodeElement, discountCodeDiv);
 
+  // Añadir al contenedor de información
+  discountInfoContainer.appendChild(discountTextDiv);
+  discountInfoContainer.appendChild(discountCodeDiv);
+
   // Manejo del enlace
   const discountLink = document.createElement('a');
-  discountLink.classList.add('ulta-discount-headband-link');
+  discountLink.classList.add('discount-headband-link');
   discountLink.href = linkUrl;
   discountLink.innerHTML = linkText;
   moveInstrumentation(linkTextElement, discountLink);
 
   // Estructura final del bloque
-  discountContent.appendChild(discountTextDiv);
-  discountContent.appendChild(discountCodeDiv);
+  discountContent.appendChild(discountInfoContainer);
   discountContent.appendChild(discountLink);
 
   // Reemplazar el contenido original del bloque con la nueva estructura
